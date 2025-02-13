f77bf13b5a3635d4adec5107ac18ac816c5b1fea
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-02-13 12:51:21 +0600

    added

---
+++ java.js
@@ -0,0 +1,46 @@
+// frist question
+
+var apple =350;
+var oranges=350;
+var total = (apple + oranges);
+var taka = 1000;
+var back = (taka - total);
+
+
+console.log(back)
+
+// second question
+
+let mathematics = 75.25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35.45;
+let bangla = 99.50;
+
+let totals = mathematics + biology + chemistry + physics + bangla;
+let averageMarks = totals / 5;
+  
+console.log(averageMarks.toFixed(2));
+
+// thrid question
+
+let y = 119;
+let remainder = y % 5;
+console.log(remainder)
+
+// Fourth question
+const b =isNaN('luffy') 
+console.log(b)
+const a = isNaN(2-10);
+console.log(a)
+
+let x = 10;
+if (x > 5) {
+  if (x < 15) {
+    console.log("x is between 5 and 15");
+  } else {
+    console.log("x is greater than or equal to 15");
+  }
+} else {
+  console.log("x is 5 or less");
+}
